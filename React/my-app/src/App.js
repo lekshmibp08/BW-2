d81e4fae96c09dcb47710bc28659e4df2e49842d
Lekshmi <sreelekshmi310192@gmail.com>
2025-08-27 19:40:04 +0530

    error boundary component added

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,8 +1,15 @@
-import UseMemoExample from "./UseMemoExample";
+import React, { Suspense } from "react";
+import ErrorBoundary from "./ErrorBoundary";
+
+const UseMemoExample = React.lazy(() => import("./UseMemoExample"));
 
 function App() {
   return (
-    <UseMemoExample/>
+    <ErrorBoundary>
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <UseMemoExample />
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
--- React/my-app/src/UseMemoExample.jsx
+++ React/my-app/src/UseMemoExample.jsx
@@ -1,44 +1,37 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState } from 'react'
 
 const UseMemoExample = () => {
-  const [search, setSearch] = useState("");
   const [count, setCount] = useState(0);
+  const [search, setSearch] = useState('');
 
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const nums = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-  const filteredNumbers = useMemo(() => {
-    console.log("Filtering numbers..."); // will run only when search changes
-    return numbers.filter((num) => num.toString().includes(search));
+  const filteredNums = useMemo(() => {
+    console.log("Rerendering...");
+    return nums.filter((num) =>
+    num.toString().includes(search))
   }, [search]);
+
   return (
-    <div className="p-4">
-      <h2 className="text-xl font-bold">useMemo Example</h2>
+    <div>
+      <h2>UseMemo Example</h2>
 
-      <input
-        type="text"
-        placeholder="Search number..."
+      <input type="text"
         value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        className="border p-2 rounded mt-2"
+        onChange={(e) => setSearch(e.target.value)}      
       />
-
-      <button
-        onClick={() => setCount(count + 1)}
-        className="ml-2 px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Increment Count ({count})
+      <button onClick={()=>setCount(count+1)}>
+        Change State Count ({count}) 
       </button>
 
-      <div className="mt-4">
-        <h3 className="font-semibold">Filtered Numbers:</h3>
-        <ul>
-          {filteredNumbers.map((num) => (
-            <li key={num}>{num}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+      {filteredNums.map((num, i) => (
+        <li key={i}>{num}</li>
+      ))}
+      </ul>
+      
     </div>
-  );
-};
+  )
+}
 
-export default UseMemoExample;
+export default UseMemoExample
